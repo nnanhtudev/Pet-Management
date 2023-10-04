f21289b8e884b7b23ad46ba20f758676d0f10614
nnanhtudev <nnanhtu.dev@gmail.com>
2023-10-05 01:05:35 +0700

    update breed.js && create edit.js

--- script.js
+++ script.js
@@ -22,52 +22,42 @@ let id, Name, age, type, weight, length, color, breed, vaccinated, dewormed, ste
 let buttonCalculate = false;
 let petArr = [];
 const storedData = getFromStorage("petArr");
-
 if (storedData) {
-  console.log(storedData);
+  console.log("🚀 ~ file: script.js:27 ~ storedData:", storedData);
+
   try {
     petArr = JSON.parse(storedData);
   } catch (error) {
     console.error("Error parsing JSON:", error);
   }
 }
-
 let healthyCheck = false;
-let healthyPetArr = [
-  {
-    id: "2",
-    Name: "Jony",
-    age: "15",
-    type: "Cat",
-    weight: "5",
-    length: "15",
-    color: "#a83434",
-    breed: "Mixed Breed",
-    vaccinated: true,
-    dewormed: true,
-    sterilized: true,
-    date: "2023-09-29T17:21:16.926Z",
-  },
-];
-
-function calculateBmiPet(pet) {
-  if (pet.type === "Dog") {
-    return (pet.weight * 703) / pet.length ** 2;
-  } else {
-    return (pet.weight * 886) / pet.length ** 2;
+let healthyPetArr = [];
+const dataHealthyPetArr = getFromStorage("healthyPetArr");
+if (dataHealthyPetArr) {
+  console.log("🚀 ~ file: script.js:27 ~ dataHealthyPetArr:", dataHealthyPetArr);
+  try {
+    healthyPetArr = JSON.parse(dataHealthyPetArr);
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
   }
 }
+
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  const date = new Date(dateString);
+  return date.toLocaleDateString("vi-VN", options);
+}
+
 function getDataPet() {
   id = inputId.value;
   Name = inputName.value;
   age = inputAge.value;
   type = inputType.value;
-  console.log("🚀 ~ file: script.js:68 ~ getDataPet ~ type:", type);
   weight = inputWeight.value;
   length = inputLength.value;
   color = inputColor1.value;
   breed = inputBreed.value;
-  console.log("🚀 ~ file: script.js:73 ~ getDataPet ~ breed:", breed);
   vaccinated = inputVaccinated.checked;
   dewormed = inputDewormed.checked;
   sterilized = inputSterilized.checked;
@@ -87,13 +77,95 @@ function getDataPet() {
     date,
   };
 }
-
-function formatDate(dateString) {
-  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-  const date = new Date(dateString);
-  return date.toLocaleDateString("vi-VN", options);
+// Calculate BMI Pet
+function calculateBmiPet(pet) {
+  if (pet.type === "Dog") {
+    return (pet.weight * 703) / pet.length ** 2;
+  } else {
+    return (pet.weight * 886) / pet.length ** 2;
+  }
 }
-
+/**
+ * The function toggles a boolean variable, clears a table, updates a button's text content, and
+ * renders a table based on the value of the boolean variable.
+ */
+function checkHealthyPet() {
+  healthyCheck = !healthyCheck;
+  clearTable(tBody);
+  btnShowPetHealthy.textContent = healthyCheck ? "Show All" : "Show Healthy Pet";
+  if (healthyCheck === true) {
+    renderFormTable(healthyPetArr);
+  } else {
+    renderFormTable(petArr);
+  }
+}
+function deletePetById(id) {
+  if (confirm("Are you sure you want to delete")) {
+    const petIndex = petArr.findIndex((pet) => pet.id === id);
+    const healthyPetIndex = healthyPetArr.findIndex((pet) => pet.id === id);
+    if (petIndex !== -1) {
+      petArr.splice(petIndex, 1);
+      saveToStorage("petArr", petArr);
+    } else {
+      console.log(`Pet with ID ${id} not found in petArr.`);
+    }
+    if (healthyPetIndex !== -1) {
+      healthyPetArr.splice(healthyPetIndex, 1);
+      saveToStorage("healthyPetArr", healthyPetArr);
+    } else {
+      console.log(`Pet with ID ${id} not found in healthyPetArr.`);
+    }
+    // Sau khi xóa, vẽ lại bảng
+    clearTable(tBody);
+    renderFormTable(healthyCheck ? healthyPetArr : petArr);
+  }
+}
+function clearTable(key) {
+  key.innerHTML = "";
+}
+renderFormTable(petArr);
+function renderFormTable(petArr) {
+  petArr.forEach((pet) => {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+      <th scope="row">${pet.id}</th>
+      <td>${pet.Name}</td>
+      <td>${pet.age}</td>
+      <td>${pet.type}</td>
+      <td>${pet.weight} kg </td>
+      <td>${pet.length} cm</td>
+      <td>${pet.breed}</td>
+      <td>
+        <i class="bi bi-square-fill" style="color: ${pet.color}"></i>
+      </td>
+      <td>${pet.vaccinated ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'}</td>
+      <td>${pet.dewormed ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'}</td>
+      <td>${pet.sterilized ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'}</td>
+      <td>${buttonCalculate ? calculateBmiPet(pet).toFixed(2) : "?"}</td>
+      <td>${formatDate(pet.date)}</td>
+      <td><button type="button" data-pet-id=${pet.id} class="btn btn-danger" id="btn-button">Delete</button></td>`;
+    tBody.appendChild(newRow);
+    const deleteButton = newRow.querySelector(".btn-danger");
+    deleteButton.addEventListener("click", function () {
+      const petId = deleteButton.getAttribute("data-pet-id");
+      deletePetById(petId);
+      newRow.remove();
+    });
+  });
+}
+function clearInput() {
+  inputId.value = "";
+  inputName.value = "";
+  inputAge.value = "";
+  inputType.value = "";
+  inputWeight.value = "";
+  inputLength.value = "";
+  inputColor1.value = "";
+  inputBreed.value = "";
+  inputVaccinated.checked = false;
+  inputDewormed.checked = false;
+  inputSterilized.checked = false;
+}
 function validateFormPet() {
   const formData = getDataPet();
   if (
@@ -130,95 +202,14 @@ function validateFormPet() {
   } else if (formData.breed === "Select Breed") {
     alert("Please select Breed!");
     return;
-  } else if (formData.dewormed === true && formData.sterilized === true && formData.sterilized === true) {
+  } else if (formData.dewormed === true && formData.vaccinated === true && formData.sterilized === true) {
     healthyPetArr.push(formData);
-    console.log("Arr Pet Healthy:", healthyPetArr);
+    saveToStorage("healthyPetArr", healthyPetArr);
   }
   petArr.push(formData);
   clearInput();
   saveToStorage("petArr", petArr);
-  console.log(saveToStorage(petArr));
   renderFormTable([formData]);
-  console.log("petArr", petArr);
-}
-
-function checkHealthyPet() {
-  healthyCheck = !healthyCheck;
-  console.log(healthyCheck);
-  clearTable(tBody);
-  btnShowPetHealthy.textContent = healthyCheck ? "Show All" : "Show Healthy Pet";
-  if (healthyCheck === true) {
-    renderFormTable(healthyPetArr);
-    console.log(healthyPetArr);
-  } else {
-    renderFormTable(petArr);
-  }
-}
-function clearInput() {
-  inputId.value = "";
-  inputName.value = "";
-  inputAge.value = "";
-  inputType.value = "";
-  inputWeight.value = "";
-  inputLength.value = "";
-  inputColor1.value = "";
-  inputBreed.value = "";
-  inputVaccinated.checked = false;
-  inputDewormed.checked = false;
-  inputSterilized.checked = false;
-}
-
-window.location.pathname === "./index.html" && renderFormTable(petArr);
-function renderFormTable(petArr) {
-  petArr.forEach((pet) => {
-    const newRow = document.createElement("tr");
-    newRow.innerHTML = `
-      <th scope="row">${pet.id}</th>
-      <td>${pet.Name}</td>
-      <td>${pet.age}</td>
-      <td>${pet.type}</td>
-      <td>${pet.weight} kg </td>
-      <td>${pet.length} cm</td>
-      <td>${pet.breed}</td>
-      <td>
-        <i class="bi bi-square-fill" style="color: ${pet.color}"></i>
-      </td>
-      <td>${pet.vaccinated ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'}</td>
-      <td>${pet.dewormed ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'}</td>
-      <td>${pet.sterilized ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'}</td>
-      <td>${buttonCalculate ? calculateBmiPet(pet).toFixed(2) : "?"}</td>
-      <td>${formatDate(pet.date)}</td>
-      <td><button type="button" data-pet-id=${pet.id} data-pet-healthy-id=${
-      healthyPetArr.id
-    } class="btn btn-danger">Delete</button></td>`;
-    tBody.appendChild(newRow);
-    const deleteButton = newRow.querySelector(".btn-danger");
-    deleteButton.addEventListener("click", function () {
-      const petId = deleteButton.getAttribute("data-pet-id");
-      const healthyPetId = deleteButton.getAttribute("data-pet-healthy-id");
-      deletePetById(petId);
-      deletePetById(healthyPetId);
-    });
-  });
-}
-function deletePetById(id) {
-  const index = petArr.findIndex((pet) => pet.id === id);
-  const indexHealthyPet = healthyPetArr.findIndex((healthyPet) => healthyPet.id === id);
-  if (index !== -1) {
-    petArr.splice(index, 1);
-    saveToStorage("petArr", petArr);
-  } else {
-    console.log(`Pet with ID ${id} not found in petArr.`);
-  }
-  if (indexHealthyPet !== -1) {
-    healthyPetArr.splice(indexHealthyPet, 1);
-    saveToStorage("petArr", petArr);
-  } else {
-    console.log(`Pet with ID ${id} not found in healthyPetArr.`);
-  }
-}
-function clearTable(key) {
-  key.innerHTML = "";
 }
 
 btnSubmit.addEventListener("click", function () {
--- script/breed.js
+++ script/breed.js
@@ -5,92 +5,121 @@ const inputName = document.getElementById("input-breed");
 const inputType = document.getElementById("input-type");
 
 export let breedArr = [];
-let idCounter = 1;
 
+/* This code is retrieving the value stored in the "breedArr" key from the storage using the
+`getFromStorage` function. If a value is retrieved successfully, it is logged to the console. Then,
+it tries to parse the retrieved value as JSON and assigns it to the `breedArr` variable. If there is
+an error while parsing the JSON, it is caught and logged to the console. */
+const getBreed = getFromStorage("breedArr");
+if (getBreed) {
+  console.log("🚀 ~ file: breed.js:12 ~ getBreed:", getBreed);
+  try {
+    breedArr = JSON.parse(getBreed);
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+  }
+}
+function findEmptyId(breedArr) {
+  for (let i = 1; i <= breedArr.length + 1; i++) {
+    /* The code `if (!breedArr.some((item) => item.id === i))` is checking if there is no element in
+    the `breedArr` array that has an `id` property equal to `i`. */
+    if (!breedArr.some((item) => item.id === i)) {
+      return i;
+    }
+  }
+  return breedArr.length + 1;
+}
 function getDataBreed() {
   let name = inputName.value;
   let type = inputType.value;
+  const newId = findEmptyId(breedArr);
   return {
-    id: idCounter++,
+    id: newId,
     name,
     type,
   };
 }
 
 export function renderBreed(breeds) {
-  const getBreed = getFromStorage("breedArr");
-  if (getBreed) {
-    console.log(getBreed);
-    try {
-      breedArr = JSON.parse(getBreed);
-    } catch (error) {
-      console.error("Error parsing JSON:", error);
-    }
-  }
   const inputBreed = document.getElementById("input-breed");
+  /* The code is iterating over each element in the `breeds` array and creating an `<option>` element for
+each breed. The `textContent` property of the `<option>` element is set to the `name` property of
+the current breed. Finally, the `<option>` element is appended as a child to the `inputBreed`
+element, which is a select dropdown input. This code is dynamically populating the select dropdown
+with options based on the breeds in the `breeds` array. */
   breeds.forEach((breed) => {
     const option = document.createElement("option");
     option.textContent = breed.name;
     inputBreed.appendChild(option);
   });
 }
+function clearInput() {
+  inputName.value = "";
+  inputType.value = "Select Type";
+}
+window.location.pathname === "/page/breed.html" && renderBreedTableIndex(breedArr);
 
-if (window.location.pathname === "/page/breed.html") {
-  function validateFormPetal() {
-    let formData = getDataBreed();
-    if (formData.name == "" || formData.type == "Select Type") {
-      alert("Name and Type cannot be empty");
-      return;
-    } else if (!isNaN(formData.name)) {
-      alert("Do not enter numbers in Name");
-      return;
-    }
-    breedArr.push(formData);
-    clearInput();
-    console.log(breedArr);
-    renderBreedTableIndex(breedArr);
-    saveToStorage("breedArr", breedArr);
-  }
-
-  function clearInput() {
-    inputName.value = "";
-    inputType.value = "Select Type";
-  }
-  renderBreedTableIndex(breedArr);
-  function renderBreedTableIndex(breedArr) {
-    const tbodyBreed = document.getElementById("tbody");
-    tbodyBreed.innerHTML = "";
-    breedArr.forEach((breed) => {
-      const newRow = document.createElement("tr");
-      newRow.innerHTML = `
+//renderBreedTableIndex id tbody
+function renderBreedTableIndex(breedArr) {
+  const tbodyBreed = document.getElementById("tbody");
+  tbodyBreed.innerHTML = "";
+  /* The code `breedArr.sort((a, b) => a.id - b.id);` is sorting the `breedArr` array in ascending
+  order based on the `id` property of each element. */
+  breedArr.sort((a, b) => a.id - b.id);
+  breedArr.forEach((breed) => {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
       <td>${breed.id}</td>
       <td>${breed.name}</td>
       <td>${breed.type}</td>
       <td><button type="button" data-breed-id=${breed.id}  class="btn btn-danger">Delete</button></td>
       `;
-      tbodyBreed.appendChild(newRow);
-      const deleteButton = newRow.querySelector(".btn-danger");
-      deleteButton.addEventListener("click", function () {
-        const breedId = deleteButton.getAttribute("data-breed-id");
-        console.log(breedId);
-        deleteBreedById(breedId);
-      });
+    tbodyBreed.appendChild(newRow);
+    const deleteButton = newRow.querySelector(".btn-danger");
+    deleteButton.addEventListener("click", function () {
+      const breedId = deleteButton.getAttribute("data-breed-id");
+      deleteBreedById(breedId);
+      alert(`Delete ${breedId} success`);
     });
-  }
-  function deleteBreedById(id) {
-    const index = breedArr.findIndex((breed) => breed.id == id);
-    if (confirm("Are you sure you want to delete")) {
-      if (index !== -1) {
-        breedArr.splice(index, 1);
-        saveToStorage("breedArr", breedArr);
-        renderBreedTableIndex(breedArr);
-      } else {
-        console.log(`Breed with ID ${id} not found in BreedArr.`);
-      }
-    }
-  }
-  btnSubmit.addEventListener("click", function () {
-    validateFormPetal();
-    console.log("Submit success");
   });
 }
+// deleteBreedById(id)
+function deleteBreedById(id) {
+  const index = breedArr.findIndex((breed) => breed.id == id);
+  if (confirm("Are you sure you want to delete")) {
+    /* The code block `if (index !== -1) { breedArr.splice(index, 1); saveToStorage("breedArr",
+   breedArr); renderBreedTableIndex(breedArr);` is responsible for deleting a breed from the
+   `breedArr` array. */
+    if (index !== -1) {
+      breedArr.splice(index, 1);
+      saveToStorage("breedArr", breedArr);
+      renderBreedTableIndex(breedArr);
+    } else {
+      console.log(`Breed with ID ${id} not found in BreedArr.`);
+    }
+  }
+}
+
+function validateFormPetal() {
+  let formData = getDataBreed();
+  if (formData.name == "" || formData.type == "Select Type") {
+    alert("Name and Type cannot be empty");
+    return;
+  } else if (!isNaN(formData.name)) {
+    alert("Do not enter numbers in Name");
+    return;
+  }
+  breedArr.push(formData);
+  clearInput();
+  console.log(breedArr);
+  /* The code `window.location.pathname === "/page/breed.html" && renderBreedTableIndex(breedArr);` is
+  checking if the current URL path is "/page/breed.html". If it is, then it calls the
+  `renderBreedTableIndex` function with the `breedArr` array as an argument. This code is
+  conditionally rendering the breed table only on the "/page/breed.html" page. */
+  window.location.pathname === "/page/breed.html" && renderBreedTableIndex(breedArr);
+  saveToStorage("breedArr", breedArr);
+}
+
+btnSubmit.addEventListener("click", function () {
+  window.location.pathname === "/page/breed.html" && validateFormPetal();
+});
